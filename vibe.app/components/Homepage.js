d17f091dd13c1dd8b2c107e9eced08ab75a7aef0
jaraJulieta <jarajuli13@gmail.com>
2024-02-17 03:00:26 -0300

    primeros pasos homepage

---
+++ vibe.app/components/Homepage.js
@@ -0,0 +1,14 @@
+import './Homepage.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Homepage = () => {
+    return (
+        <div>
+            <div className="title"> Vi_Be.ok</div>
+
+        </div>
+
+    )
+}
+
+export default Homepage;
