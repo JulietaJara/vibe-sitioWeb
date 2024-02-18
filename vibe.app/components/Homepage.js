98545b57c9f12d57cec141f6cc16ef65afc3879b
jaraJulieta <jarajuli13@gmail.com>
2024-02-18 01:27:12 -0300

    logo pantalla principal

--- vibe.app/components/Homepage.js
+++ vibe.app/components/Homepage.js
@@ -1,14 +1,27 @@
 import './Homepage.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Image, StyleSheet, View } from 'react-native';
 
 const Homepage = () => {
     return (
-        <div className='container'>
-            <div className="title h2"> Vi_Be.ok</div>
-
-        </div>
-
+        <View style={styles.container}>
+        <Image source={require('../images/logo_vibe.png')} style={styles.imagenDeFondo} />
+    </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 50, // Ajusta la distancia desde la parte superior según tus necesidades
+    },
+    imagenDeFondo: {
+      width: 200, // Ajusta el ancho según tus necesidades
+      height: 200, // Ajusta la altura según tus necesidades
+      resizeMode: 'contain', // Ajusta según tus necesidades
+    },
+  });
 
 export default Homepage;
