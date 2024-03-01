8b4e5c26c45f881450a3ac34bcda101fa1a79648
jaraJulieta <jarajuli13@gmail.com>
2024-03-01 00:43:42 -0300

    mejorando carrusel

--- vibe.app/components/CarouselProducts.js
+++ vibe.app/components/CarouselProducts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import Swiper from 'react-native-swiper';
 
 const CarouselProducts = () => {
@@ -7,22 +7,30 @@ const CarouselProducts = () => {
     { id: 1, text: 'Slide 1', image: require('../images/mate1.png') },
     { id: 2, text: 'Slide 2', image: require('../images/taza1.png') },
     { id: 3, text: 'Slide 3', image: require('../images/jabonera1.png') },
+    { id: 4, text: 'Slide 4', image: require('../images/jabonera1.png') },
   ];
+
+  const itemWidth = Dimensions.get('window').width; // Ancho del item
 
   return (
     <Swiper
+      style={styles.wrapper}
       index={0}
       loop={false}
       showsPagination
       dot={<View style={styles.paginationDot} />}
       activeDot={<View style={[styles.paginationDot, styles.activeDot]} />}
     >
-      {slides.map((slide) => (
-        <View key={slide.id} style={styles.slide}>
-          {slide.image && (
-            <Image source={slide.image} style={styles.image} resizeMode="contain" />
-          )}
-          <Text>{slide.text}</Text>
+      {[0, 1].map((index) => (
+        <View key={index} style={styles.slide}>
+          {slides.slice(index * 4, (index + 1) * 4).map((slide) => (
+            <View key={slide.id} style={styles.imageContainer}>
+              {slide.image && (
+                <Image source={slide.image} style={styles.image} resizeMode="contain" />
+              )}
+              <Text>{slide.text}</Text>
+            </View>
+          ))}
         </View>
       ))}
     </Swiper>
@@ -30,13 +38,20 @@ const CarouselProducts = () => {
 };
 
 const styles = StyleSheet.create({
+  wrapper: {},
   slide: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '25%', // Ancho de cada imagen
+  },
   image: {
-    width: '70%', 
+    width: '100%', 
     height: '70%', 
   },
   paginationDot: {
